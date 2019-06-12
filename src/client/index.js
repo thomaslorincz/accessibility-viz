71556630265884d20838bfe5270a7af7e51c8bc7
Thomas Lorincz <tlorincz@ualberta.ca>
2019-06-12 14:06:49 -0600

    Added client index files

---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+import 'core-js';
+import 'regenerator-runtime/runtime';
+import './style.css';
+
+// Needed for Hot Module Replacement
+if (typeof(module.hot) !== 'undefined') {
+  module.hot.accept();
+}
+
+// TODO: Initiate the app
